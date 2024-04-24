7ca4ac80b32624079e32d52acafe46c79cf46c1b
lucenguyen <83016168+lucenguyen@users.noreply.github.com>
2024-04-25 02:17:20 +0700

    save text in excel

--- src/export-excel/export-excel.controller.ts
+++ src/export-excel/export-excel.controller.ts
@@ -1,4 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { ExportExcelService } from './export-excel.service';
 
 @Controller('export-excel')
-export class ExportExcelController {}
+export class ExportExcelController {
+  constructor(private readonly excelService: ExportExcelService) {}
+
+  @Get()
+  getAudioData(): string {
+    return this.excelService.export();
+  }
+}
--- src/export-excel/export-excel.service.ts
+++ src/export-excel/export-excel.service.ts
@@ -1,4 +1,37 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 @Injectable()
-export class ExportExcelService {}
+export class ExportExcelService {
+
+    export(): string {
+        let text = 'Đây là demo';
+
+        const ExcelJS = require('exceljs');
+        const workbook = new ExcelJS.Workbook();
+
+        const worksheet = workbook.addWorksheet('Sheet1');
+        worksheet.addRow(['Text']);
+        worksheet.addRow(['Đây là một ví dụ về đoạn văn bản']);
+
+        workbook.xlsx.writeFile('example.xlsx')
+            .then(() => {
+                console.log('Excel file saved.');
+            })
+            .catch((error) => {
+                console.error('Error saving Excel file:', error);
+            });
+
+        return text;
+    }
+
+    // async fetchData(): Promise<string> {
+    //   try {
+    //     const response = await axios.get('https://api.example.com/data');
+    //     // return response.data;
+    //     return 'Đây là demo';
+    //   } catch (error) {
+    //     throw new Error('Failed to fetch data');
+    //   }
+    // }
+}
